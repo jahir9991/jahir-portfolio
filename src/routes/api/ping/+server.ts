07d60af8f3a3a0513dd1faa9b44ea411b1828cba
jahir alam <work.jahiralam@gmail.com>
2023-08-19 16:47:47 +0600

    90

---
+++ src/routes/api/ping/+server.ts
@@ -0,0 +1,20 @@
+import { json } from '@sveltejs/kit';
+import { MYENV } from '../../../MYENV';
+import { ENV_CHNAGE_FROM_CF, ENV_FROM_DOT } from '$env/static/private';
+
+export async function POST(full) {
+
+
+    return json(full)
+
+}
+
+export async function GET(full) {
+
+
+    return json({
+        ENV_FROM_DOT,
+        ENV_CHNAGE_FROM_CF
+    })
+
+}
