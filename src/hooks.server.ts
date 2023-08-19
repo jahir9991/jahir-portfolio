503da74e136a5df5fb3fa076e985c3ed1690bd0c
JAHIR ALAM RAJU <WORK.JAHIRALAM@GMAIL.COM>
2023-08-19 16:26:05 +0600

    Mm (#3)
    * mm merge
    
    * ok

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -2,22 +2,36 @@ import type { Handle } from "@sveltejs/kit";
 import { drizzle } from "drizzle-orm/d1";
 
 export const handle: Handle = async ({ event, resolve }) => {
-    try {
-        
-        if (event.url.pathname.startsWith('/api')
-            && event.platform.env.jahir_db
-        ) {
-            event.locals.db = drizzle(event.platform.env.jahir_db);
-            console.log("🚀 ~ file: hooks.server.ts:9 ~ consthandle:Handle= ~ event.platform.env.jahir_db:", event.platform.env.jahir_db)
-        }
+  try {
 
-    } catch (error) {
-        console.log("🚀 ~ file: hooks.server.ts:12 ~ consthandle:Handle= ~ error:", error)
+    if (event.url.pathname.startsWith('/api')) {
+      if (event.request.method === 'OPTIONS') {
+        return new Response(null, {
+          headers: {
+            'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, PATCH, OPTIONS',
+            'Access-Control-Allow-Origin': '*',
+            'Access-Control-Allow-Headers': '*',
+          }
+        });
+      }
+
+      if (event.platform.env.jahir_db) {
+        event.locals.db = drizzle(event.platform.env.jahir_db);
+        console.log("🚀 ~ file: hooks.server.ts:9 ~ consthandle:Handle= ~ event.platform.env.jahir_db:", event.platform.env.jahir_db)
+      }
 
     }
 
+  } catch (error) {
+    console.log("🚀 ~ file: hooks.server.ts:12 ~ consthandle:Handle= ~ error:", error)
 
-    const response = await resolve(event);
+  }
 
-    return response
+
+  const response = await resolve(event);
+  if (event.url.pathname.startsWith('/api')) {
+    response.headers.append('Access-Control-Allow-Origin', `*`);
+  }
+  return response;
 }
+
--- svelte.config.js
+++ svelte.config.js
@@ -13,12 +13,12 @@ const config = {
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
 
 		adapter: adapter(
-            // {
-            // routes: {
-                  //     include: ['/*'],
-                  //     exclude: ['<all>']
-                  // }
-          // }
+			{
+				routes: {
+					include: ['/*'],
+					exclude: ['<all>']
+				}
+			}
 		)
 	}
 };
